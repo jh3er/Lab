9fbe4b6c3b941e4554516cf7c6db6ebb81ab1cab
Josh <renaelworkspaces@gmail.com>
2017-11-28 18:30:03 +0700

    home for guest front end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,16 +39,14 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="/">
-                    <img src="asset/logo.svg">
-                </a>
+    
+                    <img class="navbar-brand" src="img/logo.svg">
+             
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
+                
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
@@ -78,5 +76,13 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+    <div class="container-fluid text-center">
+        <div class="fixed-bottom" style="margin-top:25px; background-color:Gray;">
+            <br>
+            <p>SneakeLLs © 2017| your daily dose | follow us</p> 
+        </div>
+    </div>
+
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <img src="asset/bg.jpg">
+        <div class="col-md-10 ">
+            <img src="img/bg.jpg" width="900" height="700">
         </div>
     </div>
 </div>
